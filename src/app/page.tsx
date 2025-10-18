c5597d293b5ba83530789f0d1de9d827e1d9facb
Kajaltiwari892 <kanjal55567@gmail.com>
2025-10-18 19:08:24 +0530

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+
+import { requireAuth } from "@/lib/auth-utils";
 
 const Page = async () => {
-  const queryClient = getQueryClient();
-
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
-
+await requireAuth()  
+  
   return (
     <>
       <div className="min-h-screen min-w-screen flex items-center">
-        <HydrationBoundary state={dehydrate(queryClient)}>
-          <Suspense fallback={<div>Loading...</div>}>
-          <Client />
-          </Suspense>
-        </HydrationBoundary>
+       protected server component
       </div>
     </>
   );
 };
 
 export default Page;
+
